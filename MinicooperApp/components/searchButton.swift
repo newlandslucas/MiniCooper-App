433dbcb29a68c0505bcb9909ed67a8ca4781ed3b
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-10-11 12:40:56 -0300

    feat: animation implemented on Search Butotn

--- MinicooperApp/components/searchButton.swift
+++ MinicooperApp/components/searchButton.swift
@@ -8,31 +8,54 @@
 import SwiftUI
 
 struct searchButton: View {
+    @State var animationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            HStack {
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("ENCONTRE A")
-                        .font(.system(size: 12))
+        Button {
+            print("clicou")
+//            animationAmount += 1
+        } label: {
+            VStack {
+                HStack {
                     
-                    Text("SUA CONCESSIONÁRIA")
-                        .font(.system(size: 15))
-                    .fontWeight(.semibold)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("ENCONTRE A")
+                            .font(.system(size: 12))
+                        
+                        Text("SUA CONCESSIONÁRIA")
+                            .font(.system(size: 15))
+                        .fontWeight(.semibold)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "arrow.forward.circle.fill")
+                        .font(.system(size: 38))
+                        .foregroundColor(.accentColor)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.accentColor)
+                                .scaleEffect(animationAmount)
+                                .opacity(2 - animationAmount)
+                                .animation(.easeInOut(duration: 1)
+                                    .repeatForever(autoreverses: true),
+                                           value: animationAmount
+                            
+                        )
+                    )
+                        .onAppear {
+                            animationAmount = 2
+                        }
                 }
-                
-                Spacer()
-                
-                Image(systemName: "map.circle.fill")
-                    .font(.system(size: 35))
-                    .foregroundColor(.accentColor)
-
+                .padding()
             }
-            .padding()
+            .frame(width: 350, height: 75)
+            .foregroundColor(.black)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(45)
         }
-        .frame(width: 350, height: 75)
-        .background(Color.gray.opacity(0.2))
-        .cornerRadius(45)
+
+
     }
 }
 
